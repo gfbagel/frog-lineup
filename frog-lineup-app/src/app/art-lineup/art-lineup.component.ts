6da3d8f0fc96fb92a5c4259096bb3bca34b1ea6e
chordanlarock <chordanlarock@gmail.com>
2024-06-24 00:58:08 -0400

    Setup images and height lines

--- frog-lineup-app/src/app/art-lineup/art-lineup.component.ts
+++ frog-lineup-app/src/app/art-lineup/art-lineup.component.ts
@@ -1,121 +1,64 @@
-import { Component, ElementRef, ViewChildren } from '@angular/core';
 import {
-  Character,
-  Rank,
-  StatMod,
-} from '../character-details/character-details.component';
+  Component,
+  ViewChildren,
+  AfterViewInit,
+  QueryList,
+  ElementRef,
+} from '@angular/core';
+import { Character } from '../character-details/character-details.component';
+import { characterList } from './characterData';
+import { FastAverageColor } from 'fast-average-color';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-art-lineup',
   standalone: true,
-  imports: [],
+  imports: [AsyncPipe],
   templateUrl: './art-lineup.component.html',
   styleUrl: './art-lineup.component.scss',
 })
-export class ArtLineupComponent {
-  @ViewChildren('imgContainer') imgContainers?: ElementRef<HTMLDivElement>[];
+export class ArtLineupComponent implements AfterViewInit {
+  @ViewChildren('line') lines?: QueryList<ElementRef<HTMLDivElement>>;
+  @ViewChildren('imgContainer') imgContainers?: QueryList<
+    ElementRef<HTMLDivElement>
+  >;
+  @ViewChildren('img') imgs?: QueryList<ElementRef<HTMLImageElement>>;
+
+  ngAfterViewInit() {
+    this.imgs?.forEach(async (img, idx) => {
+      if (this.lines!.get(idx)) {
+        this.lines!.get(idx)!.nativeElement.style.backgroundColor =
+          await this._getAvgColor(img.nativeElement);
+      }
+    });
+  }
+
+  _getAvgColor(img: HTMLImageElement) {
+    const fac = new FastAverageColor();
+    return fac
+      .getColorAsync(img)
+      .then((color) => {
+        return color.rgba;
+      })
+      .catch((e) => {
+        console.log(e);
+        return '';
+      });
+  }
   onImgLeave() {
-    this.imgContainers?.forEach((imgDiv) => {
+    this.imgContainers?.forEach((imgDiv, idx) => {
       imgDiv.nativeElement.classList.remove('not-hovered');
+      this.lines?.get(idx)?.nativeElement.classList.remove('focus-line');
     });
   }
   onImgHover(hoverIdx: number) {
     this.imgContainers?.forEach((imgDiv, idx) => {
       if (idx !== hoverIdx) {
         imgDiv.nativeElement.classList.add('not-hovered');
+      } else {
+        this.lines?.get(idx)?.nativeElement.classList.add('focus-line');
       }
     });
   }
-  characterList: Character[] = [
-    {
-      name: 'Grododu',
-      description: '',
-      ability: {
-        name: 'Genius',
-        description: '',
-        statAffecting: 'intelligence',
-        statValue: 0,
-        statModifier: StatMod.ADD,
-      },
-      weakness: {
-        name: 'Disfigured',
-        description: '',
-        statAffecting: 'dexterity',
-        statValue: 0,
-        statModifier: StatMod.SUBTRACT,
-      },
-      stats: {
-        strength: 2,
-        dexterity: 2,
-        constitution: 3,
-        intelligence: 6,
-        wisdom: 4,
-        charisma: 3,
-        luck: 5,
-      },
-      age: 0,
-      height: 50,
-      rank: Rank.CORPORAL,
-    },
-    {
-      name: 'Grododu 2',
-      description: '',
-      ability: {
-        name: 'Genius',
-        description: '',
-        statAffecting: 'intelligence',
-        statValue: 0,
-        statModifier: StatMod.ADD,
-      },
-      weakness: {
-        name: 'Disfigured',
-        description: '',
-        statAffecting: 'dexterity',
-        statValue: 0,
-        statModifier: StatMod.SUBTRACT,
-      },
-      stats: {
-        strength: 2,
-        dexterity: 2,
-        constitution: 3,
-        intelligence: 6,
-        wisdom: 4,
-        charisma: 3,
-        luck: 5,
-      },
-      age: 0,
-      height: 60,
-      rank: Rank.CORPORAL,
-    },
-    {
-      name: 'Grododu 3',
-      description: '',
-      ability: {
-        name: 'Genius',
-        description: '',
-        statAffecting: 'intelligence',
-        statValue: 0,
-        statModifier: StatMod.ADD,
-      },
-      weakness: {
-        name: 'Disfigured',
-        description: '',
-        statAffecting: 'dexterity',
-        statValue: 0,
-        statModifier: StatMod.SUBTRACT,
-      },
-      stats: {
-        strength: 2,
-        dexterity: 2,
-        constitution: 3,
-        intelligence: 6,
-        wisdom: 4,
-        charisma: 3,
-        luck: 5,
-      },
-      age: 0,
-      height: 55,
-      rank: Rank.CORPORAL,
-    },
-  ];
+  characterList: Character[] = characterList;
 }
